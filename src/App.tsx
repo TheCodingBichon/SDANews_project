94423bbe6ec1be886b52c54a56fa095f892c6ccc
TheCodingBichon <93611775+TheCodingBichon@users.noreply.github.com>
2023-01-11 21:50:57 +0100

    Stan 11.01.2023

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import React from "react";
-
+import { useState } from "react";
+import Navbar from "./components/Navbar/Navbar";
 function App() {
-  return <div className="App"></div>;
+  const [avatarClicked, setAvatarClicked] = useState<boolean>(false);
+  console.log(avatarClicked);
+  return (
+    <div className="App">
+      <Navbar
+        avatarClicked={avatarClicked}
+        setAvatarClicked={setAvatarClicked}
+      />
+    </div>
+  );
 }
 
 export default App;
+
+// 4. Przy kliknięciu na avatar: jeżeli stan avatarClicked jest ustawiony na false to zmień na true. Jeżeli jest ustawiony na true to zmień na false. Stwórz do tego odpowiednią funkcję i przypisz ją do onClicka na avatarze.
